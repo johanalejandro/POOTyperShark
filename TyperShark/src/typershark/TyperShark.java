edd491c9db27df668ed183adb3087ede8552ea69
Dario Ntn Carpio <Dario Ntn Carpio@DarNtn>
2016-08-01 14:19:51 -0500

    creación del proyecto javaFX

---
+++ TyperShark/src/typershark/TyperShark.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package typershark;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Dario Carpio, Johan Canales, Kevin Palacios
+ */
+public class TyperShark extends Application {
+    
+    @Override
+    public void start(Stage stage) {
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}

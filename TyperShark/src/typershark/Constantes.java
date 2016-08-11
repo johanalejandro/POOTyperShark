0febca80f9ffd74320e7155d50074aadc7f6bac4
Dario Ntn Carpio <Dario Ntn Carpio@DarNtn>
2016-08-11 15:44:50 -0500

    creacion de la clase Constantes

---
+++ TyperShark/src/typershark/Constantes.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package typershark;
+
+/**
+ *
+ * @author Dario Ntn Carpio
+ */
+public class Constantes {
+    public static final int VEL_TIBURON = 75;
+    public static final int VEL_PIRAÑA = 30;
+    public static final double WIDTH_SHARK = 200;
+    public static final double HEIGHT_SHARK = 70;
+    public static final double WIDTH_PIRAGNE = 70;
+    public static final double HEIGHT_PIRAGNE = 60;
+    public static final int SCREEN_WIDTH = 960;
+    public static final int SCREEN_HEIGHT = 670;
+}

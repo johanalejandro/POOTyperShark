9e50eb40181b7a6a6b286fc5df2cef4070f7db00
johanalejandro <johanalejandro@me.com>
2016-08-12 19:58:26 -0500

    Creacion clase piraña

---
+++ TyperShark/src/Personajes/Piraña.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Personajes;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import typershark.Constantes;
+
+/**
+ *
+ * @author Dario Ntn Carpio
+ */
+public class Piraña extends Animal{
+    private final int puntos = 55;    
+    
+    public Piraña(){        
+        super(Constantes.VEL_PIRAÑA);
+        int x, y;
+        x = (int)(Math.random()*250) + 620;
+        y = (int)(Math.random()*150) + 5;
+        this.setPuntos(puntos);
+        this.setPosicionInicial(x, y);
+        this.setAnimal(getLetraRandom(), new ImageView(new Image("/imagenes/piraña.png",Constantes.WIDTH_PIRAGNE,Constantes.HEIGHT_PIRAGNE,true,true)));
+    }
+    
+    private String getLetraRandom(){
+        int random = (int)(Math.random()*25)+97;
+        return ""+(char)random;
+    }
+    
+    public int getPuntos(){
+        return puntos;
+    }
+}

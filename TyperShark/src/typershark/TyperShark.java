6413ecfca916f2d57332df435e986bb5973b745e
mrkevinomar <kopalaci@espol.edu.ec>
2016-08-12 23:58:46 -0500

    Update TyperShark.java

--- TyperShark/src/typershark/TyperShark.java
+++ TyperShark/src/typershark/TyperShark.java
@@ -6,24 +6,34 @@
 package typershark;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Dario Carpio, Johan Canales, Kevin Palacios
- */
 public class TyperShark extends Application {
+
+    private static MainFrame mainFrame;
+
+    public static MainFrame getMainFrame() {
+        return mainFrame;
+    }
     
-    @Override
+    @Override 
     public void start(Stage stage) {
-        
+        Group root = new Group();
+        mainFrame = new MainFrame(root);
+        stage.setTitle("Typer Shark");
+        stage.getIcons().add(new Image("/imagenes/icon.png"));
+        stage.setResizable(false);
+        stage.setWidth(Constantes.SCREEN_WIDTH + 2*Constantes.WINDOW_BORDER);
+        stage.setHeight(Constantes.SCREEN_HEIGHT+ 2*Constantes.WINDOW_BORDER + Constantes.TITLE_BAR_HEIGHT);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        mainFrame.changeState(0);    // SPLASH = 0 
+        stage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(args);
     }
-    
-}

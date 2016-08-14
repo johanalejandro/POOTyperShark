dd6ba172a721585bd7d36564be20c43038d2b1b0
Darío Natanael Carpio Arévalo <dncarpio@fiec.espol.edu.ec>
2016-08-14 16:12:28 -0500

    Update nuevas constantes

--- TyperShark/src/typershark/Constantes.java
+++ TyperShark/src/typershark/Constantes.java
@@ -19,4 +19,14 @@ public class Constantes {
     public static final int SCREEN_WIDTH = 960;
     public static final int SCREEN_HEIGHT = 670;
     public static final Duration ANIMATION_TIME = Duration.millis(40);
+    public static final int GRUPOS_ANIMALESxNIVEL = 5;
+    public static final int MIN_ANIMALESxGRUPO = 3;
+    public static final int MAX_ANIMALESxGRUPO = 5;
+    public static final int MAX_LIVES = 3;
+    public static final int MAX_LEVEL = 5;
+    public static final int PUNTAJE_BOOSTER = 1000;
+    public static final int TITLE_BAR_HEIGHT = 19; 
+    public static final int WINDOW_BORDER = 3; 
+    public static final Color CAPTION_COLOR = Color.rgb(255, 255, 255);
+    public static final Color VALUE_COLOR = Color.rgb(0, 255, 0);
 }

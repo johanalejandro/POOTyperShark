7d3cdb2785dcbffd4432bbb83c222d77c2426dd9
Johan Canales R <johanalejandro@me.com>
2016-08-18 15:51:36 -0500

    Update LeaderBoard.java

--- TyperShark/src/typershark/LeaderBoard.java
+++ TyperShark/src/typershark/LeaderBoard.java
@@ -18,6 +18,19 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.logging.Logger;
+import javafx.beans.property.*;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.*;
+import javafx.event.EventHandler;
+import javafx.geometry.*;
+import javafx.scene.control.*;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.util.Callback;
 
 /**
  *
